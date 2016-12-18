b52a9e311e44a4164a1b4d9e0bf0eb097db8e648
zhouyihang <zhouyihangok@163.com>
2016-12-18 16:23:45 +0800

    this is a init project only.

---
+++ src/test/java/com/cti/buysell/BuySellFrameworkApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cti.buysell;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BuySellFrameworkApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
